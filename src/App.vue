1629fc409b5492fc853d4025a9c0039942eddebd
Gregory Reyes <greyes3@udi.edu.co>
2023-05-13 20:04:15 -0500

    Importación de router y llamado de account.vue Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,153 @@
 <template>
-    <div id="app" class="app">
-      <div class="header">
-        <h1>Hospital happy Pets</h1>   <!--Estaran presente durante su inicio-->
-          <nav>
+  <div id="app" class="app">
+    <div class="header">
+        <h1>Hospital happy Pets</h1>
+        <nav>
             <button v-if="is_auth" v-on:click="loadHome" > Inicio </button>
             <button v-if="is_auth" v-on:click="loadAccount"> Cuenta </button>
             <button v-if="is_auth" v-on:click="logOut"> Cerrar Sesión </button>
-            <button v-if="!is_auth" v-on:click="loadLogIn"> Iniciar Sesión </button>  <!--Estaran presentes durante su inicio-->
+            <button v-if="!is_auth" v-on:click="loadLogIn"> Iniciar Sesión </button>
             <button v-if="!is_auth" v-on:click="loadSignUp"> Registrarse </button> 
-          </nav>
-      </div>
+        </nav>
     </div>
-      <div class="main-component"> 
-        <router-view
-        v-on:completedLogIn= "completedLogIn"
-        v-on:completedSignUp= "completedSignUp"
-        v-on:logOut="logOut"
-        >
-        </router-view>
-      </div>
-      <div class="footer">
-        <h2>Derechos de autor reservados Hospitality®</h2>
-      </div>    
-  </template>
+  </div>
+  <div class="main-component">
+    <router-view 
+      v-on:completedLogIn= "completedLogIn"
+      v-on:completedSignUp= "completedSignUp"
+      v-on:logOut="logOut"
+      ></router-view>
+  </div>
+  <div class="footer">
+      <h2> Derechos de autor reservados Hospitality® </h2>
+  </div>    
+</template>
 
-  <script>
-    export default {
-      
-      data: function(){
-        return{
+<script>
+export default({
+
+  data: function(){
+      return{
         is_auth: false
-        }
-      },
-      components: {
-      },
-      
-      methods:{
-        verifyAuth: function() {  //Verifica si el usuario esta logeado
-        this.is_auth= localStorage.getItem("isAuth") || false;
-
-        if(this.is_auth == false)
-            this.$router.push({name:"logIn"});
-          else
-            this.$router.push({name:"home"});
-        },
-
-      loadLogIn: function(){
-        this.$router.push({name: "logIn"}) //Redirecciona a la pagina de inicio de sesion
-        },
-      loadSignUp: function(){
-        this.$router.push({name: "signUp"}) //Redirecciona a la pagina de registro
-        },
-
-      completedLogIn: function(data) {}, //Funciones que se ejecutaran cuando se complete el inicio de sesion o el registro
-      completedSignUp: function(data) {}, //Se ejecutaran en el componente hijo
-      },
-
-      created: function(){ //Se ejecuta cuando se crea el componente
-        this.verifyAuth()
       }
-      }
-  </script>
+  },
 
-  <style>
-    body{
-      margin: 0 0 0 0;
-    }
-    .header{  /*Estilos de los componentes*/
-      margin: 0%;
-      padding: 0;
-      width: 100%;
-      height: 10vh;
-      min-height: 100px;
-      background-color: #77a8d9 ;
-      color:#E5E7E9 ;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-    .header h1{ 
-      width: 20%;
-      text-align: center;
-      }
-    .header nav {  
-      height: 100%;
-      width: 20%;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      font-size: 20px;
-    }
-    .header nav button{
-      color: #E5E7E9;
-      background: #77a8d9;
-      border: 1px solid #E5E7E9;
-      border-radius: 5px;
-      padding: 10px 20px;
-    }
-    .header nav button:hover{
-      color: #283747;
-      background: #E5E7E9;
-      border: 1px solid #E5E7E9;
-    }
-    .main-component{ /*Estilos del componente principal*/
-      height: 75vh;
-      margin: 0%;
-      padding: 0%;
-      background: #FDFEFE ;
-    }
-    .footer{ 
-      margin: 0;
-      padding: 0;
-      height: 10vh;
-      min-height: 100px;
-      background-color: #77a8d9;
-      color: #FDFEFE;
-    }
-    .footer h2{
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  </style>
+  methods:{
+    veryAuth: function(){
+      this.is_auth= localStorage.getItem("isAuth") || false;
+
+      if(this.is_auth== false)
+        this.$router.push({name:"logIn"});
+      else
+        this.$router.push({name:"home"});
+    },  
+    loadLogIn: function(){
+        this.$router.push({name:"logIn"})
+    },
+    loadSignUp: function(){
+        this.$router.push({name:"signUp"})
+    },
+
+    completedLogIn: function(data){
+        
+        localStorage.setItem("isAuth", true);
+        localStorage.setItem("username", data.username);
+        localStorage.setItem("token_access", data.token_acess);
+        localStorage.setItem("token_refresh", data.token_refresh);
+        alert("Auntentificación Exitosa");
+        this.veryAuth();
+    },
+
+    completedSignUp: function(data){
+        alert("Registro Exitoso");
+        this.completedLogIn(data);
+    },
+    logOut:function(){
+      localStorage.clear();
+      alert("Sesión cerrada");
+      this.veryAuth();
+    },
+    loadHome:function(){
+      this.$router.push({name:"home"});
+    },
+    loadAccount: function () {
+    this.$router.push({ name: "account" });
+    },
+
+  },
+
+  created:function(){
+    this.veryAuth();
+  }
+})
+</script>
+
+
+<style>
+
+body{
+  margin: 0 0 0 0; 
+}
+
+.header{
+  margin: 0;
+  padding: 0;
+  width:100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #77a8d9;
+  color: #E5E7E9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header h1{
+  width: 50%;
+  text-align: center;
+}
+
+.header nav button
+{
+  color: #E5E7E9;
+  background: #77a8d9;
+  border: 1px solid #E5E7E9;
+  border-radius: 5px;
+  padding: 10px 20px;
+}
+
+.header nav button:hover
+{
+  color: #77a8d9;
+  background: #E5E7E9;
+  border: 1px solid #E5E7E9;
+}
+
+.main-component
+{
+  height: 75vh;
+  margin: 0%;
+  padding: 0%;
+  background: #FDFEFE;
+}
+
+.footer
+{
+  margin:0;
+  padding:0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #77a8d9;
+  color: #E5E7E9;
+}
+
+.footer h2{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
